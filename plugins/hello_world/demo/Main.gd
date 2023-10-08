8bd8bca5986794ce1d8edbd6bbd170ad86881659
Fredia Huya-Kouadio <fhuya@meta.com>
2023-10-08 14:31:43 -0700

    Update the 'HelloWorld' sample plugin directory structure

--- plugins/hello_world/demo/Main.gd
+++ plugins/hello_world/demo/Main.gd
@@ -3,7 +3,7 @@ extends Node2D
 var hello_world_plugin : HelloWorldPlugin
 
 func _ready():
-	hello_world_plugin = preload("res://addons/hello_world_plugin/interface/hello_world_plugin.gd").new()
+	hello_world_plugin = preload("res://addons/hello_world_plugin/hello_world_plugin_interface.gd").new()
 
 
 func _on_Button_pressed():
