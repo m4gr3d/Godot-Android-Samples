99e128c0a5795ce3823b23b8064f988c9b2e19d8
Fredia Huya-Kouadio <fhuya@meta.com>
2023-10-12 23:25:44 -0700

    Add logic to init Openxr xr interface

--- apps/xr_gltf_viewer/src/main/assets/main.gd
+++ apps/xr_gltf_viewer/src/main/assets/main.gd
@@ -1,11 +1,50 @@
 extends Node3D
 
+var xr_interface: XRInterface = null
+
+# Reference to the gltf model that's currently being shown.
+var current_gltf_node: Node3D = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_initialize_xr_interface()
+	
+	# Default asset to load when the app starts
+	_load_gltf_model("res://gltfs/food_kit/turkey.glb")
+	
+	_connect_to_app_plugin()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	if current_gltf_node == null:
+		return
+	
+	# Make the gltf model slowly rotate
+	current_gltf_node.rotate_y(0.001)
+
+
+func _initialize_xr_interface() -> void:
+	xr_interface = XRServer.find_interface("OpenXR")
+	if xr_interface and xr_interface.is_initialized():
+		var vp := get_viewport()
+		vp.use_xr = true
+
+
+func _load_gltf_model(gltf_path: String) -> void:
+	if current_gltf_node != null:
+		remove_child(current_gltf_node)
+	
+	current_gltf_node = load(gltf_path).instantiate()
+	add_child(current_gltf_node)
+
+
+func _connect_to_app_plugin() -> void:
+	var app_plugin := Engine.get_singleton("AppPlugin")
+	if app_plugin:
+		print("App plugin is available")
+		
+		# Signal fired from the app logic to update the gltf model being shown
+		app_plugin.connect("show_gltf", _load_gltf_model)
+	else:
+		print("App plugin is not available")
--- apps/xr_gltf_viewer/src/main/java/fhuya/godot/app/android/xr/gltfviewer/XRActivity.kt
+++ apps/xr_gltf_viewer/src/main/java/fhuya/godot/app/android/xr/gltfviewer/XRActivity.kt
@@ -2,10 +2,21 @@ package fhuya.godot.app.android.xr.gltfviewer
 
 import android.os.Bundle
 import org.godotengine.godot.GodotActivity
+import org.godotengine.godot.xr.XRMode
 
 class XRActivity : GodotActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun getCommandLine() : List<String> {
+        val commandLines = mutableListOf<String>()
+        commandLines.addAll(super.getCommandLine())
+
+        commandLines.add(XRMode.OPENXR.cmdLineArg)
+        commandLines.add("--xr-mode")
+        commandLines.add("on")
+        return commandLines
     }
 }
